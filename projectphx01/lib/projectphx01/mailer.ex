0f57dc445011d8a2e184c2a797ced34e820d6260
odiegosilva1 <odiegosilvadev@gmail.com>
2025-01-03 15:49:44 -0300

    rm:project

--- projectphx01/lib/projectphx01/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Projectphx01.Mailer do
-  use Swoosh.Mailer, otp_app: :projectphx01
-end
--- projectphx01/lib/projectphx01_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule Projectphx01Web.PageController do
-  use Projectphx01Web, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
